f74526be2afd461d55dac7281bb4b9ccc2858850
Thibault-Lambert <thibault.lambert@etu.univ-tours.fr>
2020-05-25 15:08:09 +0200

    Suppression des Boutons Imports + Documentation

--- src/main/java/fr/polytech/recognition/controller/ArticleCaractController.java
+++ src/main/java/fr/polytech/recognition/controller/ArticleCaractController.java
@@ -1,5 +1,6 @@
 package fr.polytech.recognition.controller;
 
+import fr.polytech.recognition.controller.event.events.ImageChoosenEvent;
 import fr.polytech.recognition.controller.infra.ControllerRegistration;
 import fr.polytech.recognition.controller.infra.Router;
 import fr.polytech.recognition.model.ArticleCaractModel;
@@ -8,6 +9,8 @@ import fr.polytech.recognition.view.ArticleCaractView;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -23,6 +26,11 @@ public class ArticleCaractController extends Controller<ArticleCaractView, Artic
         setView(rooter.createView(ArticleCaractView.class));
     }
 
+    /**
+     * Appellé lors du click sur un article dans l'écran ImageChosen. Ajoute les données de l'article selectionné dans le Jtab.
+     * @param tabArticle Jtab sur lequel les données sont affichés
+     * @param artSelected Article selectionné dans le Jtab d'ImageChosen
+     */
     public void populateArticleTab(JTable tabArticle, Article artSelected)
     {
         DefaultTableModel tableMod = (DefaultTableModel) tabArticle.getModel();
