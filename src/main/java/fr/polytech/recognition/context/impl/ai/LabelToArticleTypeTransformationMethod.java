bacb05b60223711384a68d57d05a14c2a2adf271
jolann <jolann.odiot@etu.univ-tours.fr>
2020-05-30 11:54:03 +0200

    Transformation method first iteration

--- src/main/java/fr/polytech/recognition/context/impl/ai/LabelToArticleTypeTransformationMethod.java
+++ src/main/java/fr/polytech/recognition/context/impl/ai/LabelToArticleTypeTransformationMethod.java
@@ -1,13 +1,17 @@
 package fr.polytech.recognition.context.impl.ai;
 
+import fr.polytech.recognition.ai.Prediction;
 import fr.polytech.recognition.ai.RecognitionResult;
 import fr.polytech.recognition.context.TransformationMethod;
 import fr.polytech.recognition.controller.infra.di.Inject;
 import fr.polytech.recognition.dao.article.ArticleDao;
+import fr.polytech.recognition.dao.article.impl.HibernateArticleDao;
 import fr.polytech.recognition.model.database.Article;
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+import org.tensorflow.op.sparse.SparseReduceMax;
 
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class LabelToArticleTypeTransformationMethod implements TransformationMethod<String, Article> {
 
@@ -16,6 +20,30 @@ public class LabelToArticleTypeTransformationMethod implements TransformationMet
 
     @Override
     public List<Article> apply(RecognitionResult<String> recognitionResult) {
-        return Collections.emptyList();//TODO
+        ArrayList<Article> foundArticle = new ArrayList<Article>();
+
+        Session session = new Configuration().configure().buildSessionFactory().openSession();
+        HibernateArticleDao dao = new HibernateArticleDao(session);
+        Collection<Article> articles = dao.getAll();
+
+        Iterator<Prediction<String>> predIt = recognitionResult.getPredictions().iterator();
+        // for every label
+        while (predIt.hasNext()) {
+            Prediction<String> prediction = predIt.next();
+            // remove bad predictions
+            if (prediction.getValue() < 0.3) continue;
+            Iterator<Article> articleIterator = articles.iterator();
+            // for every article type
+            while(articleIterator.hasNext()) {
+                Article article = articleIterator.next();
+                String articleName = article.getName();
+                System.out.println("db article name : " + articleName);
+                // if article type matches label
+                if (prediction.getObject().contains(articleName)) foundArticle.add(article);
+            }
+        }
+
+
+        return foundArticle;//TODO
     }
 }
