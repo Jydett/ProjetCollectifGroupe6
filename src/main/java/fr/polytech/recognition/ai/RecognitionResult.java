2100351b3247ff776f37aefceac62567693056dd
jolann <jolann.odiot@etu.univ-tours.fr>
2020-06-03 19:50:48 +0200

    Ajouté des commentaires (RecognitionResult)

--- src/main/java/fr/polytech/recognition/ai/RecognitionResult.java
+++ src/main/java/fr/polytech/recognition/ai/RecognitionResult.java
@@ -7,10 +7,21 @@ import java.util.ArrayList;
  * @param <O> The parameter (see prediciton class) representing the object associated with the prediction score
  */
 public interface RecognitionResult<O> {
-
+    /**
+     *
+     * @return The Peediction associated with the best score
+     */
     Prediction<O> getBestMatch();
 
+    /**
+     * Sorts the predictions
+     * @param highToLow true : sorts from highest to lowest, false : sorts from lowest to highest
+     */
     void sortPredictions(boolean highToLow);
 
+    /**
+     *
+     * @return The list of predictions.
+     */
     ArrayList<Prediction<O>> getPredictions();
 }
